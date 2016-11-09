d1d7fa38c6f54c19581f0e7a0714db43ea7fa600
Ronauli S <ronayumik@gmail.com>
2016-11-09 13:21:58 +0700

    fixing:

--- silacakThreaded/env.py
+++ silacakThreaded/env.py
@@ -1,8 +1,8 @@
 
-DB_HOST = 'localhost'
+DB_HOST = '10.199.14.30'
 DB_DATABASE = 'pelacakan'
 DB_USER = 'sa'
-DB_PASSWORD = 'Password123'
+DB_PASSWORD = 'ar4rfv.'
 
 DB_TABLE_NAME = 'publikasi_dosen'
 DB_ROW_NAME = 'pub_detilkodepub'
